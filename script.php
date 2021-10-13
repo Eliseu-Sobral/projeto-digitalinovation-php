eca0034b3711bc5b98a1139f4629a21549562847
Eliseu-Sobral <eliseusobralsantos28@gmail.com>
2021-10-12 22:16:44 -0300

    implementação de validação de dados

--- script.php
+++ script.php
@@ -6,15 +6,45 @@ $categorias[] = "Adolescente";
 $categorias[] = "Adulto";
 $categorias[] = "Idoso";
 
-
+$msg_nome = "O nome não pode estar vazio";
+$msg_idade = "A idade não pode estar vazia";
+$err_nome = "O nome é muito pequeno";
+$err_nome2 = "O nome é muito Grande";
+$err_idade = "A idade deve ser entre 0 e 120 anos ";
+$err_idade2 = "Tu já Morreu Brother";
 $nome = $_POST["nome"];
 $idade = $_POST['idade'];
 $i = 0;
-if($idade >= 6 && $idade <= 12){
+
+//Validação de dados
+if(empty($nome)){
+    echo '<script>alert("'.$msg_nome.'");</script>';
+    return;
+}elseif(empty($idade)){
+    echo '<script>alert("'.$msg_idade.'");</script>';
+    return;
+}
+
+if(strlen($nome)<3){
+    echo '<script>alert("'.$err_nome.'");</script>';
+    return;
+}elseif($idade>120){
+    echo '<script>alert("'.$err_idade.'");</script>';
+    echo '<script>alert("'.$err_idade2.'");</script>';
+    return;
+
+}elseif(strlen($nome)>40){
+    echo '<script>alert("'.$err_nome2.'");</script>';
+    return;
+}
+
+//processamento de dados
+if($idade>=6 && $idade<=12){
 
     for($i = 0; $i <= count($categorias) ; $i++){
 
-        if($categorias [$i] == "infantil") {
+        if($categorias[$i] == "infantil")
+        {
             print 'O nadador '.$nome. ' compete na categoria ' .$categorias[$i];
         }
             
@@ -23,17 +53,18 @@ if($idade >= 6 && $idade <= 12){
 }else if($idade >= 13 && $idade <=18){
 
     for($i = 0; $i <= count($categorias); $i++){
-    if($categorias[$i] == "Adolescente"){
-        echo 'O nadador, '. $nome.', compete na categora '.$categorias[$i];
-    }
+        if($categorias[$i] == "Adolescente")
+        {
+            echo 'O nadador, '. $nome.', compete na categora '.$categorias[$i];
+        }
         
     
 }
 }else if($idade >=19 && $idade <= 59){
 
     for($i = 0; $i <= count($categorias); $i++){
-    if($categorias[$i] == "Adolescente"){
-        echo 'O nadador '. $nome. ', compete na categora '.$categorias[$i];
+        if ($categorias[$i] == "Adolescente"){
+            echo 'O nadador '. $nome. ', compete na categora '.$categorias[$i];
     }
         
     
